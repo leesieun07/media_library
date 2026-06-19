c417c48260817a8d6e79f7bec05b54a06e174e11
sieun07 <lion101027@gmail.com>
2026-06-20 01:31:23 +0900

    feat: run test and fix code style adjustments

--- book_package/subclass.py
+++ book_package/subclass.py
@@ -4,10 +4,9 @@ from .core import BookSearchSystem
 class DetailedBookSearch(BookSearchSystem):
     """네이버 API 연동 및 장바구니 기능을 확장한 상세 도서 검색 클래스"""
 
-    def __init__(self, system_name, location, client_id=None, client_secret=None):
-        # 부모 클래스(BookSearchSystem)의 생성자 호출
+    def __init__(self, system_name, location,
+                 client_id=None, client_secret=None):
         super().__init__(system_name)
-        
         self.location = location
         self.client_id = client_id
         self.client_secret = client_secret
@@ -24,13 +23,16 @@ class DetailedBookSearch(BookSearchSystem):
     def search_via_api(self, keyword):
         """네이버 Book 검색 API를 호출하여 도서 리스트를 반환"""
         import requests
-        
-        url = f"https://openapi.naver.com/v1/search/book.json?query={keyword}&display=50"
+
+        url = (
+            f"https://openapi.naver.com/v1/search/book.json"
+            f"?query={keyword}&display=50"
+        )
         headers = {
             "X-Naver-Client-Id": self.client_id,
             "X-Naver-Client-Secret": self.client_secret
         }
-        
+
         try:
             res = requests.get(url, headers=headers)
             if res.status_code == 200:
